c414946a8f8109abff6d26f06d4feff7e6387f95
Tirth Shah <shahtirth0212@gmail.com>
2024-06-19 20:18:06 +0530

    Edited vercel.json

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const path = require("path");
 require("dotenv/config");
 
 const APP = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const FOOD_ROUTES = require("./routes/foods");
 const ORDER_ROUTES = require("./routes/orders");
 
